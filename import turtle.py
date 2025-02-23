1a434e510681877d8c443439f943364f290a5f02
RANKblank36 <tendosana@gmail.com>
2025-02-23 08:32:10 +0200

    Add files via upload

---
+++ import turtle.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("White")
+board = turtle.Turtle()
+
+#square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+turtle.done()
